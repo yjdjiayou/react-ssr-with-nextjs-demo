b905acbab7e611751aeed4a8ac3924278d4b857f
yjdjiayou <674032206@qq.com>
2019-11-19 17:34:07 +0800

    a

--- pages/index.js
+++ pages/index.js
@@ -1,66 +1,66 @@
-import { useEffect } from 'react'
-import { Button, Icon, Tabs } from 'antd'
-import getCofnig from 'next/config'
-import { connect } from 'react-redux'
-import Router, { withRouter } from 'next/router'
-import LRU from 'lru-cache'
+import {useEffect} from 'react';
+import {Button, Icon, Tabs} from 'antd';
+import getCofnig from 'next/config';
+import {connect} from 'react-redux';
+import Router, {withRouter} from 'next/router';
+import LRU from 'lru-cache';
 
-import Repo from '../components/Repo'
-import { cacheArray } from '../lib/repo-basic-cache'
+import Repo from '../components/Repo';
+import {cacheArray} from '../lib/repo-basic-cache';
 
-const api = require('../lib/api')
+const api = require('../lib/api');
 
 // const cache = new LRU({
 //   maxAge: 1000 * 10,
 // })
 
-const { publicRuntimeConfig } = getCofnig()
+const {publicRuntimeConfig} = getCofnig();
 
-let cachedUserRepos, cachedUserStaredRepos
+let cachedUserRepos, cachedUserStaredRepos;
 
-const isServer = typeof window === 'undefined'
+const isServer = typeof window === 'undefined';
 
-function Index({ userRepos, userStaredRepos, user, router }) {
-  // console.log(userRepos, userStaredRepos)
+function Index({userRepos, userStaredRepos, user, router}) {
+    // console.log(userRepos, userStaredRepos)
 
-  const tabKey = router.query.key || '1'
+    const tabKey = router.query.key || '1';
 
-  const handleTabChange = activeKey => {
-    Router.push(`/?key=${activeKey}`)
-  }
+    const handleTabChange = activeKey => {
+        Router.push(`/?key=${activeKey}`);
+    };
 
-  useEffect(() => {
-    if (!isServer) {
-      cachedUserRepos = userRepos
-      cachedUserStaredRepos = userStaredRepos
-      // if (userRepos) {
-      //   cache.set('userRepos', userRepos)
-      // }
-      // if (userStaredRepos) {
-      //   cache.set('userStaredRepos', userStaredRepos)
-      // }
-      const timeout = setTimeout(() => {
-        cachedUserRepos = null
-        cachedUserStaredRepos = null
-      }, 1000 * 60 * 10)
-    }
-  }, [userRepos, userStaredRepos])
+    useEffect(() => {
+        if (!isServer) {
+            cachedUserRepos = userRepos;
+            cachedUserStaredRepos = userStaredRepos;
+            // if (userRepos) {
+            //   cache.set('userRepos', userRepos)
+            // }
+            // if (userStaredRepos) {
+            //   cache.set('userStaredRepos', userStaredRepos)
+            // }
+            const timeout = setTimeout(() => {
+                cachedUserRepos = null;
+                cachedUserStaredRepos = null;
+            }, 1000 * 60 * 10);
+        }
+    }, [userRepos, userStaredRepos]);
 
-  useEffect(() => {
-    if (!isServer) {
-      cacheArray(userRepos)
-      cacheArray(userStaredRepos)
-    }
-  })
+    useEffect(() => {
+        if (!isServer) {
+            cacheArray(userRepos);
+            cacheArray(userStaredRepos);
+        }
+    });
 
-  if (!user || !user.id) {
-    return (
-      <div className="root">
-        <p>亲，您还没有登录哦~</p>
-        <Button type="primary" href={publicRuntimeConfig.OAUTH_URL}>
-          点击登录
-        </Button>
-        <style jsx>{`
+    if (!user || !user.id) {
+        return (
+            <div className="root">
+                <p>亲，您还没有登录哦~</p>
+                <Button type="primary" href={publicRuntimeConfig.OAUTH_URL}>
+                    点击登录
+                </Button>
+                <style jsx>{`
           .root {
             height: 400px;
             display: flex;
@@ -69,40 +69,40 @@ function Index({ userRepos, userStaredRepos, user, router }) {
             align-items: center;
           }
         `}</style>
-      </div>
-    )
-  }
+            </div>
+        )
+    }
 
-  return (
-    <div className="root">
-      <div className="user-info">
-        <img src={user.avatar_url} alt="user avatar" className="avatar" />
-        <span className="login">{user.login}</span>
-        <span className="name">{user.name}</span>
-        <span className="bio">{user.bio}</span>
-        <p className="email">
-          <Icon type="mail" style={{ marginRight: 10 }} />
-          <a href={`mailto:${user.email}`}>{user.email}</a>
-        </p>
-      </div>
-      <div className="user-repos">
-        {/* {userRepos.map(repo => (
+    return (
+        <div className="root">
+            <div className="user-info">
+                <img src={user.avatar_url} alt="user avatar" className="avatar"/>
+                <span className="login">{user.login}</span>
+                <span className="name">{user.name}</span>
+                <span className="bio">{user.bio}</span>
+                <p className="email">
+                    <Icon type="mail" style={{marginRight: 10}}/>
+                    <a href={`mailto:${user.email}`}>{user.email}</a>
+                </p>
+            </div>
+            <div className="user-repos">
+                {/* {userRepos.map(repo => (
           <Repo repo={repo} />
         ))} */}
-        <Tabs activeKey={tabKey} onChange={handleTabChange} animated={false}>
-          <Tabs.TabPane tab="你的仓库" key="1">
-            {userRepos.map(repo => (
-              <Repo key={repo.id} repo={repo} />
-            ))}
-          </Tabs.TabPane>
-          <Tabs.TabPane tab="你关注的仓库" key="2">
-            {userStaredRepos.map(repo => (
-              <Repo key={repo.id} repo={repo} />
-            ))}
-          </Tabs.TabPane>
-        </Tabs>
-      </div>
-      <style jsx>{`
+                <Tabs activeKey={tabKey} onChange={handleTabChange} animated={false}>
+                    <Tabs.TabPane tab="你的仓库" key="1">
+                        {userRepos.map(repo => (
+                            <Repo key={repo.id} repo={repo}/>
+                        ))}
+                    </Tabs.TabPane>
+                    <Tabs.TabPane tab="你关注的仓库" key="2">
+                        {userStaredRepos.map(repo => (
+                            <Repo key={repo.id} repo={repo}/>
+                        ))}
+                    </Tabs.TabPane>
+                </Tabs>
+            </div>
+            <style jsx>{`
         .root {
           display: flex;
           align-items: flex-start;
@@ -136,66 +136,66 @@ function Index({ userRepos, userStaredRepos, user, router }) {
           flex-grow: 1;
         }
       `}</style>
-    </div>
-  )
+        </div>
+    )
 }
 
-Index.getInitialProps = async ({ ctx, reduxStore }) => {
-  // const result = await axios
-  //   .get('/github/search/repositories?q=react')
-  //   .then(resp => console.log(resp))
+Index.getInitialProps = async ({ctx, reduxStore}) => {
+    // const result = await axios
+    //   .get('/github/search/repositories?q=react')
+    //   .then(resp => console.log(resp))
 
-  const user = reduxStore.getState().user
-  console.log(reduxStore)
-  if (!user || !user.id) {
+    const user = reduxStore.getState().user;
+    console.log(reduxStore);
+    if (!user || !user.id) {
+        return {
+            isLogin: false,
+        }
+    }
+
+    if (!isServer) {
+        // if (cache.get('userRepos') && cache.get('userStaredRepos')) {
+        //   return {
+        //     userRepos: cache.get('userRepos'),
+        //     userStaredRepos: cache.get('userStaredRepos'),
+        //   }
+        // }
+
+        if (cachedUserRepos && cachedUserStaredRepos) {
+            return {
+                userRepos: cachedUserRepos,
+                userStaredRepos: cachedUserStaredRepos,
+            }
+        }
+    }
+
+    const userRepos = await api.request(
+        {
+            url: '/user/repos',
+        },
+        ctx.req,
+        ctx.res,
+    );
+
+    const userStaredRepos = await api.request(
+        {
+            url: '/user/starred',
+        },
+        ctx.req,
+        ctx.res,
+    );
+
     return {
-      isLogin: false,
+        isLogin: true,
+        userRepos: userRepos.data,
+        userStaredRepos: userStaredRepos.data,
     }
-  }
-
-  if (!isServer) {
-    // if (cache.get('userRepos') && cache.get('userStaredRepos')) {
-    //   return {
-    //     userRepos: cache.get('userRepos'),
-    //     userStaredRepos: cache.get('userStaredRepos'),
-    //   }
-    // }
-
-    if (cachedUserRepos && cachedUserStaredRepos) {
-      return {
-        userRepos: cachedUserRepos,
-        userStaredRepos: cachedUserStaredRepos,
-      }
-    }
-  }
-
-  const userRepos = await api.request(
-    {
-      url: '/user/repos',
-    },
-    ctx.req,
-    ctx.res,
-  )
-
-  const userStaredRepos = await api.request(
-    {
-      url: '/user/starred',
-    },
-    ctx.req,
-    ctx.res,
-  )
-
-  return {
-    isLogin: true,
-    userRepos: userRepos.data,
-    userStaredRepos: userStaredRepos.data,
-  }
-}
+};
 
 export default withRouter(
-  connect(function mapState(state) {
-    return {
-      user: state.user,
-    }
-  })(Index),
+    connect(function mapState(state) {
+        return {
+            user: state.user,
+        }
+    })(Index),
 )
--- server/auth.js
+++ server/auth.js
@@ -2,81 +2,78 @@ const axios = require('axios');
 
 const config = require('../config.sample');
 
-const { client_id, client_secret, request_token_url } = config.github;
+const {client_id, client_secret, request_token_url} = config.github;
 
 module.exports = server => {
-  server.use(async (ctx, next) => {
-    if (ctx.path === '/auth') {
-      const code = ctx.query.code;
-      if (!code) {
-        ctx.body = 'code not exist';
-        return
-      }
-      const result = await axios({
-        method: 'POST',
-        url: request_token_url,
-        data: {
-          client_id,
-          client_secret,
-          code,
-        },
-        headers: {
-          Accept: 'application/json',
-        },
-      });
+    server.use(async (ctx, next) => {
+        if (ctx.path === '/auth') {
+            const code = ctx.query.code;
+            if (!code) {
+                ctx.body = 'code not exist';
+                return
+            }
+            const result = await axios({
+                method: 'POST',
+                url: request_token_url,
+                data: {
+                    client_id,
+                    client_secret,
+                    code,
+                },
+                headers: {
+                    Accept: 'application/json',
+                },
+            });
 
-      console.log(result.status, result.data);
+            if (result.status === 200 && (result.data && !result.data.error)) {
+                ctx.session.githubAuth = result.data;
 
-      if (result.status === 200 && (result.data && !result.data.error)) {
-        ctx.session.githubAuth = result.data;
+                const {access_token, token_type} = result.data;
 
-        const { access_token, token_type } = result.data;
+                const userInfoResp = await axios({
+                    method: 'GET',
+                    url: 'https://api.github.com/user',
+                    headers: {
+                        Authorization: `${token_type} ${access_token}`,
+                    },
+                });
 
-        const userInfoResp = await axios({
-          method: 'GET',
-          url: 'https://api.github.com/user',
-          headers: {
-            Authorization: `${token_type} ${access_token}`,
-          },
-        });
+                ctx.session.userInfo = userInfoResp.data;
 
-        // console.log(userInfoResp.data)
-        ctx.session.userInfo = userInfoResp.data;
+                ctx.redirect((ctx.session && ctx.session.urlBeforeOAuth) || '/');
+                ctx.session.urlBeforeOAuth = ''
+            } else {
+                const errorMsg = result.data && result.data.error;
+                ctx.body = `request token failed ${errorMsg}`
+            }
+        } else {
+            await next()
+        }
+    });
 
-        ctx.redirect((ctx.session && ctx.session.urlBeforeOAuth) || '/');
-        ctx.session.urlBeforeOAuth = ''
-      } else {
-        const errorMsg = result.data && result.data.error;
-        ctx.body = `request token failed ${errorMsg}`
-      }
-    } else {
-      await next()
-    }
-  });
+    server.use(async (ctx, next) => {
+        const path = ctx.path;
+        const method = ctx.method;
+        if (path === '/logout' && method === 'POST') {
+            ctx.session = null;
+            ctx.body = `logout success`
+        } else {
+            await next()
+        }
+    });
 
-  server.use(async (ctx, next) => {
-    const path = ctx.path;
-    const method = ctx.method;
-    if (path === '/logout' && method === 'POST') {
-      ctx.session = null;
-      ctx.body = `logout success`
-    } else {
-      await next()
-    }
-  });
-
-  server.use(async (ctx, next) => {
-    const path = ctx.path;
-    const method = ctx.method;
-    if (path === '/prepare-auth' && method === 'GET') {
-      // ctx.session = null
-      // ctx.body = `logout success`
-      const { url } = ctx.query;
-      ctx.session.urlBeforeOAuth = url;
-      // ctx.body = 'ready'
-      ctx.redirect(config.OAUTH_URL)
-    } else {
-      await next()
-    }
-  });
+    server.use(async (ctx, next) => {
+        const path = ctx.path;
+        const method = ctx.method;
+        if (path === '/prepare-auth' && method === 'GET') {
+            // ctx.session = null
+            // ctx.body = `logout success`
+            const {url} = ctx.query;
+            ctx.session.urlBeforeOAuth = url;
+            // ctx.body = 'ready'
+            ctx.redirect(config.OAUTH_URL)
+        } else {
+            await next()
+        }
+    });
 };
